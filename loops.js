672d4f3ab0f0ec8514215f705aac651ae99b31a7
Wenjie <xuwenjie8866@gmail.com>
2017-10-27 14:19:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n){
   }
   return 'done';
 }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    console.log('doo-bee-doo-bee-doo')
+  } while (maybeTrue());
+}
